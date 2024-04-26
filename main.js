9dceef3643ba9402d75b9fa797f7880cecce3a6a
J <james.a.kelley.mars@gmail.com>
2024-04-25 19:34:47 -0600

    add logging

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ const led = new Gpio(17, "out"); // Use the GPIO pin number for your LED
 let ledOn = false;
 
 function turnOnLED() {
+  console.log(`LED on`);
   led.writeSync(1); // Turn LED on
   ledOn = true;
 }
 
 function turnOffLED() {
+  console.log(`LED off`);
   led.writeSync(0); // Turn LED off
   ledOn = false;
 }
@@ -21,6 +23,7 @@ turnOnLED();
 setTimeout(() => {
   turnOffLED();
   led.unexport();
+  console.log(`LED released!`);
 }, 6000);
 
 // setInterval(toggleLED, 2000);
